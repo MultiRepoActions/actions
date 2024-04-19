a528b94d880fd233d70a79349730e37eba7c33e7
Alexander Swensen <alex.swensen@gmail.com>
2024-04-19 12:21:37 -0500

    wip on getting automation working

---
+++ src/getRepos.js
@@ -0,0 +1,46 @@
+
+
+
+
+export async function getReposForOrg(octokit, org) {
+  const repoData = await octokit.paginate( octokit.rest.repos.listForOrg, {
+    org: org,
+  });
+  return repoData.map(repo => repo.name);
+}
+
+export async function getPackagesJsonFromRepo(octokit, owner, repo) {
+  return await octokit.rest.repos.getContent({
+    owner: owner,
+    repo: repo,
+    path: 'package.json',
+  });
+}
+
+export async function getPackagesJsonFromRepoContent(octokit, owner, repo, branch) {
+  const fileApiData = await octokit.rest.repos.getContent({
+    owner: owner,
+    repo: repo,
+    path: 'package.json',
+    ref: branch || 'main',
+  });
+  const fileContent = Buffer.from(fileApiData.data.content, 'base64').toString();
+  return JSON.parse(fileContent);
+}
+
+export const getReposWithPackageJson = async (octokit, org, repos) => {
+  const reposWithPackageJson = [];
+  for (const repo of repos) {
+    try {
+      await getPackagesJsonFromRepo(octokit, org, repo);
+      reposWithPackageJson.push(repo);
+    } catch (error) {
+      console.log(`No package.json found in ${repo}`);
+    }
+  }
+  return reposWithPackageJson;
+};
+
+// getReposForOrg(octokit, '').then((repos) => {
+//     console.log(repos)
+// });
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import { Octokit } from "@octokit/rest";
+import {
+  getReposForOrg,
+  getReposWithPackageJson,
+  getPackagesJsonFromRepoContent,
+} from "./getRepos.js";
+
+let octokit;
+if (!process.env.CI) {
+  octokit = new Octokit({
+    auth: process.env.GITHUB_API_TOKEN,
+  });
+}
+
+const orgName = "MultiRepoActions";
+
+const repos = await getReposForOrg(octokit, orgName);
+
+const validRepos = await getReposWithPackageJson(octokit, orgName, repos);
+
+// update repos where the package.json has either dependencies or devDependencies that match the name of the provided package.
+
+const reposToUpdate = validRepos.map(async (repo) => {
+  const packageJson = await getPackagesJsonFromRepoContent(
+    octokit,
+    orgName,
+    repo
+  );
+  const dependencies = packageJson.dependencies || {};
+  const devDependencies = packageJson.devDependencies || {};
+  return {
+    repo,
+    dependencies,
+    devDependencies,
+  };
+});
+
+await Promise.all(reposToUpdate).then((repos) => {
+  console.log(repos);
+});
+
+// console.log(reposToUpdate);
+
+// console.log(validRepos);
